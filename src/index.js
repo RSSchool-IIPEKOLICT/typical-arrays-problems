856bb9451b81495b3a29316bce6e7a64595fdbe6
iipekolict <iipekolict@gmail.com>
2021-08-17 20:23:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(n) {
+    switch (n) {
+        case undefined: return 0
+        default: return (n.length > 0) ? n.reduce((p, c) => (p <= c) ? p : c) : 0
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(n) {
+    switch (n) {
+        case undefined: return 0
+        default: return (n.length > 0) ? n.reduce((p, c) => (p >= c) ? p : c) : 0
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(n) {
+    switch (n) {
+        case undefined: return 0
+        default: return (n.length > 0) ? n.reduce((p, c) => p + c) / n.length : 0
+    }
 }
