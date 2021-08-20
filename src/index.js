78ac0b8b8c9783f715c4ee9a3fa6d3e86b0a05d2
iipekolict <iipekolict@gmail.com>
2021-08-20 22:20:33 +0300

    fix: more clear solution

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 
 exports.min = function min(n) {
-    switch (n) {
-        case undefined: return 0
-        default: return (n.length > 0) ? n.reduce((p, c) => (p <= c) ? p : c) : 0
-    }
+    return (n !== undefined) ? (n.length > 0) ? n.reduce((p, c) => (p <= c) ? p : c) : 0 : 0
 }
 
 exports.max = function max(n) {
-    switch (n) {
-        case undefined: return 0
-        default: return (n.length > 0) ? n.reduce((p, c) => (p >= c) ? p : c) : 0
-    }
+    return (n !== undefined) ? (n.length > 0) ? n.reduce((p, c) => (p >= c) ? p : c) : 0 : 0
 }
 
 exports.avg = function avg(n) {
-    switch (n) {
-        case undefined: return 0
-        default: return (n.length > 0) ? n.reduce((p, c) => p + c) / n.length : 0
-    }
+    return (n !== undefined) ? (n.length > 0) ? n.reduce((p, c) => p + c) / n.length : 0 : 0
 }
